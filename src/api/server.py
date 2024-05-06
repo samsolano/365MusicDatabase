1afdc675e5b9181d2b4779efdcded507e7eb557e
Sam Solano <samuelsolano00@gmail.com>
2024-05-06 16:05:31 -0700

    commented shit

--- src/api/server.py
+++ src/api/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, exceptions
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from api import musicmain
+# from api import musicmain
 import json
 import logging
 import sys
@@ -34,7 +34,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(musicmain.router)
+# app.include_router(musicmain.router)
 
 
 @app.exception_handler(exceptions.RequestValidationError)
